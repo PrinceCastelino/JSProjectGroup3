5310d0e4011cb7f8dff9319e386463efbad5f9f1
Prince Castelino <pcastelino1882@conestogac.on.ca>
2023-12-14 16:38:00 -0500

    Products cart

--- cart.js
+++ cart.js
@@ -1,4 +1,8 @@
 "use strict";
+
+// Define an array to store added products
+let cartItems = [];
+
 // Function to update the cart count
 function updateCartCount(count) {
     const cartCount = document.querySelector('.cart-count');
@@ -10,6 +14,25 @@ function addItemToCart() {
     let count = parseInt(document.querySelector('.cart-count').textContent);
     count++;
     updateCartCount(count);
+
+    // Extracting product details and adding to cartItems array
+    const productDiv = this.closest('.product');
+    const image = productDiv.querySelector('img');
+    const productName = productDiv.querySelector('h2').textContent;
+    const price = productDiv.querySelector('.product-price').textContent;
+
+    const product = {
+        imageUrl: image.src,
+        productName: productName,
+        amount: price
+    };
+
+    cartItems.push(product);
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+
+    // Log the cart items to the console
+    console.log("Cart Items:");
+    console.log(cartItems);
 }
 
 // Function to initialize the cart functionality
@@ -18,9 +41,14 @@ function initializeCart() {
     addToCartButtons.forEach(button => {
         button.addEventListener('click', addItemToCart);
     });
+
+    // Retrieve cartItems from localStorage and update the cart count
+    const storedCart = localStorage.getItem('cartItems');
+    if (storedCart) {
+        cartItems = JSON.parse(storedCart);
+        updateCartCount(cartItems.length);
+    }
 }
 
 // Call the initializeCart function when the DOM content is loaded
-document.addEventListener('DOMContentLoaded', function() {
-    initializeCart();
-});
+document.addEventListener('DOMContentLoaded', initializeCart);
