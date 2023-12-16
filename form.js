03090863c7fe52953994a314e49e9b6cb6b4bfeb
PrinceCastelino <153687159+PrinceCastelino@users.noreply.github.com>
2023-12-16 10:37:31 -0500

    Add files via upload

--- form.js
+++ form.js
@@ -1,4 +1,5 @@
 "use strict";
+
 $(document).ready(() => {
    //if user press login open login form
    $('#switchToLogin').click(() => {
@@ -16,22 +17,19 @@ $(document).ready(() => {
    });
 
    function showSuccessMessage() {
-      // Display success message with animation
       $("#successMessage").text("Successfully signed up!");
       $("#successMessage").fadeIn(2000, function () {
-         // Hide the success message after 5 seconds
          setTimeout(function () {
             $("#successMessage").fadeOut(2000);
          }, 5000);
       });
      
-        
    }
-   // the handler for the click event of the submit button
+   // set the handler event for the submit button
    $("#signupForm").submit(event => {
       let isValid = true;
 
-      // validate the email entry with a regular expression
+      // verify the email entry with a regular expression
       const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
       const email = $("#email").val().trim();
 
@@ -46,7 +44,7 @@ $(document).ready(() => {
       }
       $("#email").val(email);
 
-      // validate the password entry
+      // verify the password entry
       const password = $("#password").val().trim();
       if (password.length < 6) {
          $("#password").next().text("Must be 6 or more characters.");
@@ -56,7 +54,7 @@ $(document).ready(() => {
       }
       $("#password").val(password);
 
-      // validate the verify entry
+      // verify entry
       const verify = $("#verify").val().trim();
       if (verify == "") {
          $("#verify").next().text("This field is required.");
@@ -68,33 +66,39 @@ $(document).ready(() => {
          $("#verify").next().text("");
          
       }
-      $("#verify").val(verify);
+      $("#verify").val(verify);  
 
-      $("#signupForm").submit(event => {
-       
-      });
-
-      // validate the first name entry
+      // verify the first name with the complete regular expression
       const firstName = $("#first_name").val().trim();
+      const fnamePattern = /^[^\s!@#$%^&*()_+=\-[\]{};':"\\|,.<>/?]*$/;
+
       if (firstName == "") {
          $("#first_name").next().text("This field is required.");
          isValid = false;
+      } else if (!fnamePattern.test(firstName)){
+         $("#first_name").next().text("Invalid characters in the first name.");
+         isValid = false;
       } else {
          $("#first_name").next().text("");
-      }
+      } 
       $("#first_name").val(firstName);
 
-      // validate the last name entry
+      // verify the last name with the complete regular expression
       const lastName = $("#last_name").val().trim();
+      const lnamePattern = /^[^\s!@#$%^&*()_+=\-[\]{};':"\\|,.<>/?]*$/;
+
       if (lastName == "") {
          $("#last_name").next().text("This field is required.");
+         isValid = false;
+      } else if (!lnamePattern.test(lastName)) {
+         $("#last_name").next().text("Invalid characters in the last name.");
          isValid = false;
       } else {
          $("#last_name").next().text("");
       }
       $("#last_name").val(lastName);
 
-      // validate the phone number with a regular expression
+      // verify the phone number with a complete regular expression
       const phonePattern = /^\d{3}-\d{3}-\d{4}$/;
       const phone = $("#phone").val().trim();
       if (phone == "") {
@@ -109,7 +113,7 @@ $(document).ready(() => {
       $("#phone").val(phone);
 
 
-      // prevent the submission of the form if any entries are invalid 
+      // when any entries are invalid to stop the prevent event 
       if (isValid == false) {
          event.preventDefault();
       }
@@ -124,11 +128,11 @@ $(document).ready(() => {
 
    });
 
-   // the handler for the click event of the submit button
+   // set the handler event for the submit button
    $("#loginform").submit(event => {
       let isValid = true;
 
-      // validate the email entry with a regular expression
+      // verify the email entry with a complete regular expression
       const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
       const email = $("#l_email").val().trim();
 
@@ -143,7 +147,7 @@ $(document).ready(() => {
       }
       $("#l_email").val(email);
 
-      // validate the password entry
+      // verify the password entry
       const password = $("#l_password").val().trim();
       if (password.length < 6) {
          $("#l_password").next().text("Must be 6 or more characters.");
@@ -152,38 +156,22 @@ $(document).ready(() => {
          $("#l_password").next().text("");
       }
       $("#l_password").val(password);
-
-
-      // prevent the submission of the form if any entries are invalid 
+ 
+      // when any entries are invalid to stop the prevent event 
       if (isValid == false) {
          event.preventDefault();
       }
 
       if (isValid) {
          event.preventDefault();
-         const username = 'user@example.com'; // Replace with the actual username
-         const password = 'password'; // Replace with the actual password
-
-         if (email === username && password === $("#l_password").val().trim()) {
-            // Redirect to index.html on successful login
-            window.location.href = 'index.html';
-         } else {
-            // Display an error message or handle unsuccessful login
-            console.log('Invalid credentials. Please try again.');
-            // Show the success message for demonstration purposes (Remove in production)
-            showSuccessLoginMessage();
-         }
+         showSuccessLoginMessage();
       }
 
    });
 
- 
-
    function showSuccessLoginMessage() {
-      // Display success message with animation
       $("#successMessage").text("Successfully Login!");
       $("#successMessage").fadeIn(2000, function () {
-         // Hide the success message after 5 seconds
          setTimeout(function () {
             $("#successMessage").fadeOut(2000);
          }, 5000);
